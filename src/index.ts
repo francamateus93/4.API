936e334d3107a356c59a4eaca89ebc5e3c0d6062
francamateus93 <francamateus@gmail.com>
2024-12-03 12:26:14 +0100

    Ejercio 2 - Responsive Completed

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import { fetchJoke } from './api';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const app = document.getElementById('app');
   const jokeText = document.getElementById('joke-text') as HTMLParagraphElement;
   const jokeButton = document.getElementById('joke-next') as HTMLButtonElement;
 
   const updateJoke = async () => {
-    const fetchingJoke = await fetchJoke();
-    jokeText.textContent = fetchingJoke;
-    console.log(fetchingJoke);
+    const joke = await fetchJoke();
+    jokeText.textContent = joke;
+    console.log(joke);
   }
 
   jokeButton.addEventListener('click', updateJoke);
-
   updateJoke();
 });
